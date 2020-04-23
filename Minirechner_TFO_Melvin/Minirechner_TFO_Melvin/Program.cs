e0d7be1035f59a4514dec0ba180b3678833d135e
MelvinReka <62886559+MelvinReka@users.noreply.github.com>
2020-04-23 11:10:09 +0200

    Einlesen der Zahlen

--- Minirechner_TFO_Melvin/Minirechner_TFO_Melvin/Program.cs
+++ Minirechner_TFO_Melvin/Minirechner_TFO_Melvin/Program.cs
@@ -5,10 +5,31 @@ using System.Text;
 
 namespace Minirechner_TFO_Melvin
 {
+
     class Program
     {
+        
         static void Main(string[] args)
-        {
+        {    int Zahl1, Zahl2;
+            Titel("Mirechner:");
+            Console.WriteLine();
+            Console.WriteLine("Geben sie die erste Zahl ein:");
+            Zahl1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Geben sie die zweite Zahl ein:");
+            Zahl2 = Convert.ToInt32(Console.ReadLine());
+            Console.ReadKey();
         }
+        static void Titel(string Titel) {
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine(Titel);
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                Console.Write("*");
+            }
+        }
+
     }
 }
